cd0102bb8ef22e32114be7d061dc38a094b8103b
j1gnacio <j.burgos18@ufromail.cl>
2025-03-21 14:23:01 -0300

    Terminado

---
+++ mostrar.cs
@@ -0,0 +1,8 @@
+// Método para mostrar el resultado en pantalla
+static void MostrarResultado(double promedio)
+{
+    if (promedio > 0)
+    {
+        Console.WriteLine($"El promedio de las calificaciones es: {promedio}");
+    }
+}
